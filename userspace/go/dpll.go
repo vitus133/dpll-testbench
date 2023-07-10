f6d465592007bf12777e043f6360ff4633372b91
Vitaly Grinberg <v.g@redhat.com>
2023-07-10 14:22:13 +0300

    wip

--- userspace/go/dpll.go
+++ userspace/go/dpll.go
@@ -5,9 +5,24 @@ package main
 // #include "ynl.h"
 // #include "dpll-user.h"
 import "C"
+import (
+	"fmt"
+	"log"
+)
 
+// Note - this doesn't work. Hits segfault on line 26
 func main() {
-	var ys *C.struct_ynl_sock
+	//var ys *C.struct_ynl_sock
+	var dpll_family C.struct_ynl_family
+	var yse C.struct_ynl_error
 
-	ys = C.ynl_sock_create(&C.ynl_dpll_family, C.NULL)
+	dpll_family.name = C.CString("dpll")
+	ys := C.ynl_sock_create(&dpll_family, &yse)
+	if yse.code != 0 {
+		log.Fatal("can't allocate ynl socket", yse.msg)
+	}
+	var devs *C.struct_dpll_device_get_list
+	devs = C.dpll_device_get_dump(ys)
+	ls := devs.obj._present
+	fmt.Println(ls)
 }
