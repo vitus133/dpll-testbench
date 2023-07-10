418e1ec63d76506c7e10484eefb5c26a008da0e2
Vitaly Grinberg <v.g@redhat.com>
2023-07-10 16:07:26 +0300

    dpll-list poc

--- kernel-root/tools/net/ynl/samples/dpll-list.c
+++ kernel-root/tools/net/ynl/samples/dpll-list.c
@@ -3,7 +3,8 @@
 #include <ynl.h>
 #include <libmnl/libmnl.h>
 #include <linux/genetlink.h>
-
+#include <json-c/json.h>  // dnf install json-c json-c-devel
+#include <json-c/json_object.h>  // dnf install json-c json-c-devel
 #include "dpll-user.h"
 
 
@@ -12,6 +13,7 @@ int main(int argc, char **argv)
 	struct ynl_sock *ys;
 	struct dpll_device_get_list *list;
 	struct dpll_device_get_list *next;
+	json_object *root;
 
 	int rc = 0;
 
@@ -28,14 +30,19 @@ int main(int argc, char **argv)
 		rc = 3;
 		goto err_close;
 	}
+	root = json_object_new_array();
 	next = list;
 	while(next != (struct dpll_device_get_list*)YNL_LIST_END){
-		printf("Module name: %s\n", next->obj.module_name);
-		printf("Lock status: %s\n", dpll_lock_status_str(next->obj.lock_status));
-		printf("DPLL type: %s\n", dpll_type_str(next->obj.type));
-		printf("Clock ID: %llx\n", next->obj.clock_id);
-		next = next->next;	
+		json_object *child = json_object_new_object();
+		json_object_object_add(child, "module-name", json_object_new_string(next->obj.module_name));
+		json_object_object_add(child, "lock-status", json_object_new_string(dpll_lock_status_str(next->obj.lock_status)));
+		json_object_object_add(child, "id", json_object_new_int(next->obj.id));
+		json_object_object_add(child, "type", json_object_new_string(dpll_type_str(next->obj.type)));
+		json_object_object_add(child, "clock-id", json_object_new_uint64(next->obj.clock_id));
+		next = next->next;
+		json_object_array_add(root, child);	
 	}
+	json_object_to_fd(fileno(stdout), root, 0); //JSON_C_TO_STRING_PRETTY
 	dpll_device_get_list_free(list);
 
 err_close:
