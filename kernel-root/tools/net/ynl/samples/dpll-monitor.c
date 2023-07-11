e80a3d6d61aa3ec39faff3861115e14575bc8ec0
Vitaly Grinberg <v.g@redhat.com>
2023-07-11 09:41:31 +0300

    split monitoring part from dpll

--- kernel-root/tools/net/ynl/samples/dpll-monitor.c
+++ kernel-root/tools/net/ynl/samples/dpll-monitor.c
@@ -10,8 +10,6 @@
 
 #include "dpll-user.h"
 
-#define DPLL_ID 0
-
 typedef struct{
 	int mcast_id;
 	char* mcast_name;
@@ -233,47 +231,7 @@ int ntf_main(int group)
 
 int main(int argc, char **argv)
 {
-	struct ynl_sock *ys;
-	struct dpll_device_get_rsp *dev;
-	struct dpll_device_get_req *req;
-//	struct ynl_ntf_base_type *ntf;
-	int rc = 0;
-
-	ys = ynl_sock_create(&ynl_dpll_family, NULL);
-	if (!ys){
-		fprintf(stderr,"failed to create socket\n");
-		return 1;
-	}
-
-	req = dpll_device_get_req_alloc();
-	if (!req){
-		fprintf(stderr, "failed to allocate request\n");
-		rc = 2;
-		goto sock_close;
-	}
-	
-	dpll_device_get_req_set_id(req, DPLL_ID);
-
-	dev = dpll_device_get(ys, req);
-	
-	if (!dev){
-		fprintf(stderr, "failed to get device\n");
-		rc = 3;
-		goto err_close;
-	}
-
-	printf("Module name: %s\n", dev->module_name);
-	printf("Lock status: %s\n", dpll_lock_status_str(dev->lock_status));
-	printf("DPLL type: %s\n", dpll_type_str(dev->type));
-	printf("Clock ID: %llx\n", dev->clock_id);
-	dpll_device_get_rsp_free(dev);
-
 	get_mcast_id("dpll");
 	ntf_main(4);
-	
-err_close:
-	dpll_device_get_req_free(req);
-sock_close:
-	ynl_sock_destroy(ys);
-	return rc;
+	return 0;
 }
